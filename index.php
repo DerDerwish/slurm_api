85a408d5ab0262664c848e7ffbee18296e243779
DerDerwish <derderwish@chaotikum.org>
2017-03-24 16:18:49 +0100

    Jetzt auch mit index.php (-_-)

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+// Kickstart the framework
+$f3=require('lib/base.php');
+
+function execute_command($CMD) {
+	$tmp = exec(escapeshellcmd($CMD), $out, $ret);
+	if ($ret == 0) {
+		return $out;
+	} else {
+		return false;
+	}
+}
+
+$f3->set('DEBUG',1);
+if ((float)PCRE_VERSION<7.9)
+	trigger_error('PCRE version is out of date');
+
+// Load configuration
+$f3->config('config.ini');
+
+// List all partitions
+$f3->route('GET /partitions', function() {
+	$out = execute_command('sinfo --noheader --format=%R');
+	if ($out) {
+		echo json_encode($out);
+	}
+});
+
+// Configuration of a given partition
+// TODO
+$f3->route('GET /partition/@partition_name', function() {
+	'scontrol --oneliner show partition '.$partition_name)
+	$out = execute_command('sinfo --noheader --format=%R');
+	if ($out) {
+		echo json_encode($out);
+	}
+});
+
+$f3->run();
